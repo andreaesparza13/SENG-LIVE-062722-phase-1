fd26c5b04c07a15233794b1b6133c61574069df4
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2023-10-30 14:26:08 -0500

    redo lesson 2

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -1,55 +1,43 @@
-//Declare Functions
-
-//Demo querySelector
+// Declare Functions
 function renderHeader() {
-    const header = document.querySelector("h1")
-    header.textContent = bookStore.name
+    document.querySelector("h1").textContent = bookStore.name
 }
 
-//Demo querySelectorAll
 function renderFooter() {
-    const footerDivs = document.querySelectorAll('footer div')
-    //Bookstore Name
+    const footerDivs = document.querySelectorAll("footer div")
     footerDivs[0].textContent = bookStore.name
-    //Bookstore Address
     footerDivs[1].textContent = bookStore.address
-    //Bookstore Hours
     footerDivs[2].textContent = bookStore.hours
 }
 
-//Demo Create Elements
-// function createBook() {
+function createBook() {
 
-// }
+}
 
-function renderBook(book) {
-        // Create the elements
-        const li = document.createElement('li')
-        const h3 = document.createElement('h3')
-        const pAuthor = document.createElement('p')
-        const pPrice = document.createElement('p')
-        const img = document.createElement('img')
-        const btn = document.createElement('button')
-        btn.addEventListener('click', e => {
-            li.remove()
-        })
-    
-        // Populate Elements
-        h3.textContent = book.title
-        pAuthor.textContent = book.author
-        pPrice.textContent = book.price
-        btn.textContent = "Delete Book"
-        img.src = book.imageUrl
-        li.className = 'list-li'
-    
-        li.append(h3, pAuthor, pPrice, img, btn)
-        document.querySelector("#book-list").append(li)
-    }
+const renderBook = book => {
+    // Generate HTML Elements
+    const li = document.createElement("li")
+    const h3 = document.createElement("h3")
+    const pAuthor = document.createElement("p")
+    const pPrice = document.createElement("p")
+    const img = document.createElement("img")
+    const btn = document.createElement("button")
 
+    // Fill HTML Elements
+    h3.textContent = book.title
+    pAuthor.textContent = book.author
+    pPrice.textContent = `$${book.price.toFixed(2)}`
+    img.src = book.imageUrl
+    btn.textContent = "Delete Book"
+    li.className = "list-li"
 
-//Invoke Function
+    // Generate Full <li> For Each Book
+    li.append(h3, pAuthor, pPrice, img, btn)
+    document.querySelector("#book-list").append(li)
+}
+
+// Invoke Functions
 renderHeader()
 renderFooter()
 
-const books = bookStore.inventory
-books.forEach(renderBook)
+const books = bookStore.inventory.forEach(renderBook)
